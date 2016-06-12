7dfa93cc3b50400a3282c241499f22d1dec95327
Andreas Mast <andreasmast@web.de>
2016-06-12 18:27:58 +0200

    Änderungen mit Matthi

--- Andy Remote/Steuerung/Steuerung.php
+++ Andy Remote/Steuerung/Steuerung.php
@@ -9,19 +9,19 @@
   define ('Pin_Motor2_enable',25);
 
   function set_gpio_export_out(){         //direction "out"
-    	shell_exec(gpio export 9 out);
-    	shell_exec(gpio export 10 out);
-    	shell_exec(gpio export 11 out); //Enable
-    	shell_exec(gpio export 23 out);
-    	shell_exec(gpio export 24 out);
-    	shell_exec(gpio export 25 out); //Enable
+    	shell_exec("gpio export " . Pin_Motor1_forw . " out");
+    	shell_exec("gpio export 10 out");
+    	shell_exec("gpio export 11 out"); //Enable
+    	shell_exec("gpio export 23 out");
+    	shell_exec("gpio export 24 out");
+    	shell_exec("gpio export 25 out"); //Enable
 
       echo "Export complete!";
   }
 
   function set_gpio_enable(){
-    shell_exec(gpio -g write 11 1);
-    shell_exec(gpio -g write 25 1);
+    shell_exec("gpio -g write 11 1");
+    shell_exec("gpio -g write 25 1");
 
     echo "Both Enable on!";
   }
@@ -30,43 +30,38 @@
     shell_exec(gpio -g write $pin $value);
   }
 
+  function reset_all_motor_pins(){
+    write_pin(Pin_Motor1_forw, 0);
+    write_pin(Pin_Motor1_backw, 0);
+    write_pin(Pin_Motor2_forw, 0);
+    write_pin(Pin_Motor2_backw, 0);
+  }
+
   //Programmstart
 
   set_gpio_export_out();
-  set_enable_pin();
+  set_gpio_enable();
 
-  $ButtonEingabe = $_Post['ButtonEingabe'];
+  $buttonEingabe = $_POST['ButtonEingabe'];
 
-  switch ($ButtonEingabe) {
+  switch ($buttonEingabe) {
     case 'forward':
-      write_pin(Pin_Motor1_forw, 0);
-      write_pin(Pin_Motor1_backw, 0);
-      write_pin(Pin_Motor2_forw, 0);
-      write_pin(Pin_Motor2_backw, 0);
+      reset_all_motor_pins();
       write_pin(Pin_Motor1_forw, 1);
       write_pin(Pin_Motor2_forw, 1);
       break;
     case 'backward':
-      write_pin(Pin_Motor1_forw, 0);
-      write_pin(Pin_Motor1_backw, 0);
-      write_pin(Pin_Motor2_forw, 0);
-      write_pin(Pin_Motor2_backw, 0);
+      reset_all_motor_pins();
       write_pin(Pin_Motor1_backw, 1);
       write_pin(Pin_Motor2_backw, 1);
       break;
     case 'right':
-      write_pin(Pin_Motor1_forw, 0);
-      write_pin(Pin_Motor1_backw, 0);
-      write_pin(Pin_Motor2_forw, 0);
-      write_pin(Pin_Motor2_backw, 0);
+      reset_all_motor_pins();
       write_pin(Pin_Motor1_forw, 1);
       write_pin(Pin_Motor2_backw, 1);
       break;
     case 'left':
-      write_pin(Pin_Motor1_forw, 0);
-      write_pin(Pin_Motor1_backw, 0);
-      write_pin(Pin_Motor2_forw, 0);
-      write_pin(Pin_Motor2_backw, 0);
+      reset_all_motor_pins();
       write_pin(Pin_Motor1_backw, 1);
       write_pin(Pin_Motor2_forw, 1);
       break;
